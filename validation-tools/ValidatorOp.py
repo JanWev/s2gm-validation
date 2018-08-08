bd207c50074ffd274d546cb1711eb7040527af7e
Jan_W <jan.weversr@brockmann-consult.de>
2018-08-08 14:34:20 +0200

    Added logger to ValidatorOP

---
+++ validation-tools/ValidatorOp.py
@@ -0,0 +1,32 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+""" Purpose: Orchestrate download and the validation of s2gm products.
+"""
+
+import sys
+import argparse
+import logging
+from datetime import datetime
+
+__author__ = 'jan wevers - jan.wevers@brockmann-consult.de'
+
+def main(tests):
+    print(tests)
+    logging.basicConfig(filename='execution.log', filemode='a',level=logging.DEBUG)
+    logging.info('ValidationOp executed: %s', str(datetime.now()))
+    logging.debug('Requested tests by the user: %s', str(tests))
+    logging.info('\n')
+
+if __name__ == "__main__":
+    #input_one = sys.argv[1]
+    CLI = argparse.ArgumentParser()
+    CLI.add_argument(
+        "--tests",# name on the CLI - drop the `--` for positional/required parameters
+        nargs="*",  # 0 or more values expected => creates a list
+        type=int,
+        default=[1, 2, 3], # default if nothing is provided
+        help='index numbers of wanted tests. usage: --tests 1 2 5 6. default --tests 1 2 3'
+    )
+    # parse the command line
+    args = CLI.parse_args()
+    main(args.tests)
---
+++ validation-tools/utilities/clear_log_file.py
@@ -0,0 +1,2 @@
+with open('..\example.log', 'w'):
+    pass
