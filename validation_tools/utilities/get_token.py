d2efe6ae735177837b6381db04f725080ce9fb0a
Rok Mocnik <rok.mocnik@sinergise.com>
2018-10-09 16:55:58 +0200

    implement auth token retrieval from Sentinel Hub Identity service

---
+++ validation_tools/utilities/get_token.py
@@ -0,0 +1,25 @@
+import re
+import requests
+
+from urllib.parse import urlparse
+
+IDENTITY_PATH = ("https://services.sentinel-hub.com/oauth/auth?client_id=c525444c-6bd4-4290-b56f-11b792491465"
+                 "&redirect_uri=https%3A%2F%2Fapps.sentinel-hub.com%2Fmosaic-hub%2Foauth2callback.html"
+                 "&scope=&response_type=token&state=")
+LOGIN_PATH = "https://services.sentinel-hub.com/oauth/auth/login"
+SESSION_NAME = "session"
+CSRT_PATTERN = re.compile("value=\"(.*)\" name=\"csrt\">")
+
+
+def get_token(username, password):
+    session = requests.Session()
+    resp = session.get(IDENTITY_PATH)
+    csrt = CSRT_PATTERN.findall(resp.text)
+    if not csrt:
+        exit(0)
+
+    payload = {"csrt": csrt[0], "username": username, "password": password}
+    resp = session.post(LOGIN_PATH, data=payload, allow_redirects=False)
+
+    url = urlparse(resp.headers['Location'])
+    return url.fragment.split("&")[0].split("=")[1]
