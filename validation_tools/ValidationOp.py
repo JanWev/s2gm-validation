d9663595f39644792da62b8378607ae192303434
fgirtler <girtler@geoville.com>
2018-10-11 16:57:23 +0200

    Basic structure of validation functionality

--- validation_tools/ValidationOp.py
+++ validation_tools/ValidationOp.py
@@ -10,34 +10,77 @@ from datetime import datetime, date
 
 __author__ = 'jan wevers, tanja gasber & florian girtler - jan.wevers@brockmann-consult.de, gasber@geoville.com, girtler@geoville.com'
 
-def log_inputs(tests):
+def log_inputs(tests, reference_path, validate_path):
     if not os.path.isdir('./logs'):
         os.makedirs('./logs')
     try:
         logging.basicConfig(filename='./logs/execution.log', filemode='a',level=logging.DEBUG)
-        logging.info('ValidationOp executed: %s', str(datetime.now()))
-        logging.debug('Requested tests by the user: %s', str(tests))
     except FileNotFoundError:
         print('log file does not exist and will be created')
         f = open('./logs/execution.log')
         f.close()
         logging.basicConfig(filename='./logs/execution.log', filemode='a', level=logging.DEBUG)
-        logging.info('ValidationOp executed: %s', str(datetime.now()))
-        logging.debug('Requested tests by the user: %s', str(tests))
+    logging.info('ValidationOp executed: %s', str(datetime.now()))
+    logging.debug('Requested tests by the user: %s', str(tests))
+    logging.debug('Reference: {}, to validate: {}'.format(reference_path, validate_path))
 
 
-def main(tests):
-    log_inputs(tests)
-    # trigger L0 to L3 tests
+def check_input(reference_path, validate_path):
+    #TODO: see if all pkl files exist for both reference and to-validate directory
+    #TODO: check if data are valid (e.g. reference is older than to-validate)
+    return True
+
+
+def prepare_tests(tests, reference_path, validate_path):
+    log_inputs(tests, reference_path, validate_path)
+
+    #TODO: check integrity of reference and validation folder
+    if (not check_input(reference_path, validate_path)):
+        logging.error('Input data are not valid, check log for more information.')
+        return
+    #TODO: run the different checks
+    if 'L0' in tests:
+        print('running test L0 for {}'.format(validate_path))
+        # L0: check file integrity of validate folder
+        #result = run_L0_test(validate_path)
+    if 'L1' in tests:
+        print('running test L1 for {}'.format(validate_path))
+
+
+    #TODO: create validation report
+
+
+
 
 if __name__ == "__main__":
-    CLI = argparse.ArgumentParser()
+    CLI = argparse.ArgumentParser(description = "This script retrieves Sentinel-2 metadata from copernicus hub and imports it into a database")
     CLI.add_argument(
+        "-t",
         "--tests",
-        nargs="*",
-        type=int,
-        default=[1, 2, 3],
-        help='index numbers of wanted tests. usage: --tests 1 2 5 6. default --tests 1 2 3'
+        nargs="+",
+        type=str,
+        default=['L0', 'L1', 'L2', 'L3'],
+        required=False,
+        metavar="test-ID",
+        help='IDs of tests that should be run. Can be L0, L1, L2 or L3. If omitted all available tests will be run'
+    )
+    CLI.add_argument(
+        "-r",
+        "--reference",
+        nargs=1,
+        type=str,
+        required=True,
+        metavar="path",
+        help='Path to the directory of the validation reference data'
+    )
+    CLI.add_argument(
+        "-v",
+        "--validate",
+        nargs=1,
+        type=str,
+        required=True,
+        metavar="path",
+        help='Path to the directory of the data to be validated'
     )
     args = CLI.parse_args()
-    main(args.tests)
+    prepare_tests(args.tests, args.reference, args.validate)
