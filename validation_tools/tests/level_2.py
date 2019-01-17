c39bf570e6570f8550736e66ae22b74661218a6b
Jan_W <jan.weversr@brockmann-consult.de>
2019-01-17 14:09:24 +0100

    started development on loading bands for comaprison

--- validation_tools/tests/level_2.py
+++ validation_tools/tests/level_2.py
@@ -3,8 +3,10 @@
 """ Purpose: Make L2 tests. L2 checks for similarity of products
 """
 
-
+import os
+import glob
 import gdal
+from pathlib import Path
 
 __author__ = 'jan wevers - jan.wevers@brockmann-consult.de'
 
@@ -26,18 +28,37 @@ def level_2_1(test_metadata, comparable):
             driver_name = ''
         elif test_metadata['image_format'] == 'GEO_TIFF':
             driver_name = 'GTiff'
+            file_ext = 'tiff'
         else:
             driver_name = 'JP2OpenJPEG'
-        driver = gdal.GetDriverByName('GTiff')
+            file_ext = 'jp2'
+        driver = gdal.GetDriverByName(driver_name)
         driver.Register()
-        # inData = gdal.Open(inFn)
-        # raster = inData.GetRasterBand(1)
-        # rasterAr = raster.ReadAsArray()
-        # rasterAr[rasterAr == nodata] = 0
-        # outraster = inData.GetRasterBand(1)
-        # outrasterAr = outraster.ReadAsArray().astype(np.float)
-        # outrasterAr[outrasterAr == nodata] = 0
-        # inWkt, GeoT, rows, cols, Projection, pixelRes = GetGeoInfo(inData)
+
+        valPath = test_metadata['validate_path']
+        refPath = test_metadata['reference_path']
+
+        #get subdirs names
+        valSubPaths = [f.path for f in os.scandir(valPath) if f.is_dir()]
+        refSubPaths = [f.path for f in os.scandir(refPath) if f.is_dir()]
+        # loop over tiles if any
+        for i in range(len(valSubPaths)):
+            valSubPath = valSubPaths[i]
+            refSubPath = refSubPaths[i]
+            valBands = glob.glob(valSubPath + '\*.' + file_ext)
+            refBands = glob.glob(refSubPath + '\*.' + file_ext)
+            for k in range(len(valBands)):
+                #Todo: Find out why it won't open
+                valData = gdal.Open(valBands[k])
+                #refData = gdal.Open(refBands[k])
+                valRaster = valData.GetRasterBand(1)
+                #refRaster = refData.GetRasterBand(1)
+                # rasterAr = raster.ReadAsArray()
+                # rasterAr[rasterAr == nodata] = 0
+                # outraster = inData.GetRasterBand(1)
+                # outrasterAr = outraster.ReadAsArray().astype(np.float)
+                # outrasterAr[outrasterAr == nodata] = 0
+                # inWkt, GeoT, rows, cols, Projection, pixelRes = GetGeoInfo(inData)
 
         # fill test result
         test_passed = True
