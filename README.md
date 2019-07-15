a696a23b899bce9097b794f0dd1998216f20fb7b
Jan_W <jan.weversr@brockmann-consult.de>
2019-07-15 08:48:27 +0200

    refactoring

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ The validation of S2GM products comprises 4 levels from L0 to L3
     - (potential test): Spatial difference for SR for different bands - visual check
 - L3 - Product validation
     - Compare number of input products to mosaicking
-    - Compare applied algorithm (not possible due to missing band)
+    - Compare applied algorithm
     - Compare selected date (same as L2.4 source index)
 
 
--- validation_tools/tests/level_1.py
+++ validation_tools/tests/level_1.py
@@ -1,6 +1,7 @@
 from osgeo import gdal
 from pathlib import Path
 import os
+import inspect
 
 
 """
@@ -135,6 +136,103 @@ def level_1_0(test_metadata):
                         os.remove(path + '/' + element)
             except:
                 pass
+
+            test_result['result'] = {
+                'finished': True,
+            }
+
+
+    except Exception as ex:
+        test_result['result'] = {
+            'finished': False,
+            'passed': False,
+            'error': str(ex),
+        }
+
+    print('Level 1 check done!')
+    return test_result
+
+def level_1_1(test_metadata, val_res_path):
+
+
+    test_result = {
+        'test_id': 'level_1_1',
+        'test_name': 'RGB quicklook',
+    }
+
+    try:
+        product_path = Path(test_metadata['validate_path'])
+        for subdir in product_path.iterdir():
+            path = str(product_path / subdir)
+            if subdir.is_dir():
+                tile = os.path.basename(os.path.normpath(str(subdir)))
+                rgb_list = []
+                file_dict = {}
+                tag = ''
+                i = 0
+                for file in os.listdir(path):
+                    if file.endswith('json'):
+                        tag = 'json'
+                        current_path, dir = os.path.split(os.path.dirname(__file__))
+                        print(current_path)
+                        cmd = 'pconvert -f png -H 550 -p "' + current_path + '\\aux_data\\s2Profile_B432.rgb" -o ' + val_res_path + ' ' + str(
+                            subdir) + '\\' + file
+                        print(cmd)
+                        prods_path, prod_name_temp = os.path.split(str(product_path))
+                        prod_name = prod_name_temp.rsplit('_', 1)[0]
+                    if file.endswith('tiff') or file.endswith('jp2'):
+                        if file.startswith('B'):
+                            file_dict[file[0:3]] = path + '/' + file
+                            prod_path, new_prod_name = os.path.split(str(path))
+                            try:
+                                blue_band = file_dict['B02']
+                                green_band = file_dict['B03']
+                                red_band = file_dict['B04']
+                                rgb_list = [red_band, green_band, blue_band]
+                            except:
+                                pass
+                if len(rgb_list) == 3 and i == 0 and tag == 'json':
+                    os.system(cmd)
+                    # for element in os.listdir(val_res_path):
+                    #     if element.startswith(prod_name[:-7]):
+                    #         os.rename(val_res_path + '/' + element,
+                    #                   val_res_path + '/' + new_prod_name + '.png')
+                    # os.rename(val_res_path + '/' + prod_name + '.png', val_res_path + '/' + new_prod_name + '.png')
+                    i = 1
+
+                for file in os.listdir(path):
+                    if file.endswith('.nc'):
+                        current_path, dir = os.path.split(os.path.dirname(__file__))
+                        print(current_path)
+                        cmd = 'pconvert -f png -H 550 -p "' + current_path + '\\aux_data\\s2Profile_B432.rgb" -o ' + val_res_path + ' ' + str(subdir) + '\\' + file
+                        print(cmd)
+                        img = gdal.Open(path + '/' + file)
+                        subdatasets = img.GetSubDatasets()
+                        for variable in subdatasets:
+                            if variable[0].endswith('B02'):
+                                blue_band = variable[0]
+                                blue_band = blue_band.replace('\\', '/')
+                                rgb_list.append(blue_band)
+
+                            if variable[0].endswith('B03'):
+                                green_band = variable[0]
+                                green_band = green_band.replace('\\', '/')
+                                rgb_list.insert(0, green_band)
+
+                            if variable[0].endswith('B04'):
+                                red_band = variable[0]
+                                red_band = red_band.replace('\\', '/')
+                                rgb_list.insert(0, red_band)
+
+                        if len(rgb_list) == 3:
+                            os.system(cmd)
+
+                        else:
+                            test_result['result'] = {
+                                'finished': False,
+                                'error': 'Not all required bands for RGB available (missing bands)'
+                            }
+                            continue
 
             test_result['result'] = {
                 'finished': True,
